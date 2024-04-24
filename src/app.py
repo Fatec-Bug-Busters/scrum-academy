f4ba4dc64f6798106bbadfae855e82d6650454f2
GabrielViellCastilho <gabrielviell@hotmail.com>
2024-04-24 19:34:47 -0300

    Add Links Footer/ Page Sobre Nos

--- src/app.py
+++ src/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/sobre_nos')
+def sobre_nos():
+    return render_template("sobre_nos.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
