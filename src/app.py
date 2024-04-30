03d8481e3a48124b7c8feb072290d46b4a2050ae
Wesley Gonçalves <dev@wesleygoncalves.com>
2024-04-30 14:53:17 -0300

    Prepara Página Conteúdo para os textos

--- src/app.py
+++ src/app.py
@@ -1,29 +1,37 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/sobre_nos')
+
+@app.route("/sobre_nos")
 def sobre_nos():
     return render_template("sobre_nos.html")
 
-@app.route('/exame_pg1')
+
+@app.route("/exame_pg1")
 def exame_pg1():
     return render_template("exame_pg1.html")
 
-@app.route('/exame_pg2')
+
+@app.route("/exame_pg2")
 def exame_pg2():
     return render_template("exame_pg2.html")
 
-@app.route('/exame_pg3')
+
+@app.route("/exame_pg3")
 def exame_pg3():
     return render_template("exame_pg3.html")
 
-@app.route('/conteudo')
-def conteudo():
-    return render_template("conteudo.html")
 
-if __name__ == '__main__':
+@app.route("/exemplo")
+def conteudo():
+    return render_template("conteudos/exemplo.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
