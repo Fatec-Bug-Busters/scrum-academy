f160b84c8951cf9eedfb23e76c34edd113a4e847
GabrielViellCastilho <gabrielviell@hotmail.com>
2024-05-20 20:22:34 -0300

    Session System

--- src/app.py
+++ src/app.py
@@ -1,107 +1,109 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 from datetime import date
 
 app = Flask(__name__)
-
-
+app.secret_key = "12345678"
 
 lst_user = {
-    "user1@example.com": {"NameUser": "Alice","CreatedAtDate" : "2022-12-01","id": 1},
-    "user2@example.com": {"NameUser": "Bob","CreatedAtDate" : "2022-11-01", "id": 2},
-    "user3@example.com": {"NameUser": "Charlie","CreatedAtDate" : "2022-10-01", "id": 3}
+    "user1@example.com": {"NameUser": "Alice", "CreatedAtDate": "2022-12-01", "id": 1},
+    "user2@example.com": {"NameUser": "Bob", "CreatedAtDate": "2022-11-01", "id": 2},
+    "user3@example.com": {"NameUser": "Charlie", "CreatedAtDate": "2022-10-01", "id": 3}
 }
-
-name_now = None
-
-def return_template(template, **kwargs):
-    return render_template(template, name_now=name_now, **kwargs)
 
 @app.route("/")
 def index():
-    global name_now
-    return return_template("index.html")
+    name_now = session.get('name_now')
+    return render_template("index.html", name_now=name_now)
 
 
 @app.route("/sobre_nos")
 def sobre_nos():
-    return return_template("sobre_nos.html")
+    name_now = session.get('name_now')
+    return render_template("sobre_nos.html", name_now=name_now)
 
 
 @app.route("/exame")
 def exame():
-    return return_template("exame.html")
+    name_now = session.get('name_now')
+    return render_template("exame.html", name_now=name_now)
 
 
 @app.route("/resultados")
 def resultados():
-    return return_template("resultados.html")
+    name_now = session.get('name_now')
+    return render_template("resultados.html", name_now=name_now)
 
 
 @app.route("/artefatos-e-eventos-1")
 def artefatoseeventos1():
-    return return_template("conteudos/artefatos-e-eventos-1.html")
+    name_now = session.get('name_now')
+    return render_template("conteudos/artefatos-e-eventos-1.html", name_now=name_now)
 
 
 @app.route("/introducao")
 def introducao():
-    return return_template("conteudos/introducao.html")
+    name_now = session.get('name_now')
+    return render_template("conteudos/introducao.html", name_now=name_now)
 
 
 @app.route("/artefatos-e-eventos-2")
 def artefatoseeventos2():
-    return return_template("conteudos/artefatos-e-eventos-2.html")
+    name_now = session.get('name_now')
+    return render_template("conteudos/artefatos-e-eventos-2.html", name_now=name_now)
 
 
 @app.route("/papeis-e-pilares")
 def papeisepilares():
-    return return_template("conteudos/papeis-e-pilares.html")
+    name_now = session.get('name_now')
+    return render_template("conteudos/papeis-e-pilares.html", name_now=name_now)
 
 
 @app.route("/exemplo")
 def conteudo():
-    return return_template("conteudos/exemplo.html")
+    name_now = session.get('name_now')
+    return render_template("conteudos/exemplo.html", name_now=name_now)
 
 
 @app.route("/questoes")
 def questoes():
-    return return_template("components/questoes.html")
+    name_now = session.get('name_now')
+    return render_template("components/questoes.html", name_now=name_now)
+
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        data = request.get_json()
-        email = data['email']
-        
-        # Simples verificação de usuário e senha (aqui você pode conectar ao banco de dados para uma verificação real)
-        if email in lst_user:
-            global name_now
-            name_now = lst_user[email]["NameUser"]
-            return jsonify({'success': True})
-        else:
-            return jsonify({'success': False})
+    data = request.get_json()
+    email = data['email']
+    
 
-# Rota para o registro
+    if email in lst_user:
+        session['name_now'] = lst_user[email]["NameUser"]
+        session['email'] = email
+        return jsonify({'success': True})
+    else:
+        return jsonify({'success': False})
+
+
 @app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        data = request.get_json()
-        NameUser = data['NameUser']
-        email = data['email']
-        if len(NameUser.split()) > 1:
-            lst_user[email] = {"NameUser": NameUser, "CreatedAtDate" : date.today(), "id": 4}
-        else:
-            ErrorName = ErrorName
-        global name_now
-        name_now = lst_user[email]["NameUser"]
-    return jsonify({'NameUser': NameUser, 'email': email})
+    data = request.get_json()
+    NameUser = data['NameUser']
+    email = data['email']
+    
+    if len(NameUser.split()) > 1:
+        lst_user[email] = {"NameUser": NameUser, "CreatedAtDate": date.today().strftime("%Y-%m-%d"), "id": 4}
+        session['name_now'] = NameUser
+        session['email'] = email
+        return jsonify({'NameUser': NameUser, 'email': email})
+    else:
+        return jsonify({'success': False, 'message': 'Invalid name'}), 400
 
-# Rota para o registro
+
 @app.route('/logout', methods=['POST'])
 def logout():
-    global name_now
-    if request.method == 'POST':
-        name_now = None
-    return 
+    session.pop('name_now', None)
+    session.pop('email', None)
+    return jsonify({'success': True})
 
 
 if __name__ == "__main__":
