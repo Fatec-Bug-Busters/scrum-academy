d0bfb481ca99d5925f483bc8825dcea95fd535be
Diego <diegocastilholorenceto@gmail.com>
2024-05-25 16:39:22 -0300

    Conexão com o BD feita, falta fazer a dos quiz

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from flask_mysqldb import MySQL
 import os
 import datetime
@@ -77,15 +77,57 @@ def avaliar():
 def certificado():
     return render_template("components/certificado.html")
 
+#Quizz ainda nao funcionando pois falta o interaction_id
 @app.route('/submit-score', methods=['POST'])
 def submit_score():
     data = request.json
     total_correct = data.get("totalCorrect")
     user_answers = data.get("userAnswers")
     print(f"Quantidade de questões corretas recebidas: {total_correct}")
-    return jsonify(
-        {"status": "success", "totalCorrect": total_correct, "userAnswer": user_answers}
-    )
+
+    # cursor = mysql.connection.cursor()
+    # cursor.execute(''' 
+    #     INSERT INTO quizzes (score, users_answer) 
+    #     VALUES (%s, %s) 
+    # ''', (total_correct, user_answers))
+    # mysql.connection.commit()
+    # cursor.close()
+
+    return jsonify({"status": "success", "totalCorrect": total_correct, "userAnswer": user_answers})
+
+@app.route('/submit-score-exame', methods=['POST'])
+def submit_score_exame():
+    data = request.json
+    total_correct = data.get("totalCorrect")
+    users_answer = data.get("userAnswers")
+
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('''
+            INSERT INTO exams (score, users_answer, created_at)
+            VALUES (%s, %s, NOW())
+        ''', (total_correct, users_answer))
+        mysql.connection.commit()
+        cursor.close()
+
+        return jsonify({"status": "success", "totalCorrect": total_correct, "userAnswers": users_answer})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+    
+@app.route('/submit-avaliacao', methods=['POST'])
+def submit_avaliacao():
+    comentario = request.form['comentario']
+    estrelas = request.form['fb']
+
+    print(f"Comentário: {comentario}, Estrelas: {estrelas}")
+    
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO iterations(review_comment, review_score, created_at) VALUES(%s, %s, NOW()) ''', (comentario, estrelas))
+    mysql.connection.commit()
+    cursor.close()
+
+    return redirect(url_for('resultados'))
 
 if __name__ == "__main__":
     app.run(debug=True)
