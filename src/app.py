d07943cb1fb3b412a60ec6f525faf863017d0e93
Wesley Gonçalves <dev@wesleygoncalves.com>
2024-05-26 13:56:32 -0300

    Feature/quiz conteudo 3 (#18)
    Add questões novas no Quiz
    
    Add responsividade aos modelos de questões
    Faz ajustes nos modelos de questões
    Add correção das questões
    Envia respostas para o servidor
    Move sample pages
    Cada modelo de questao pode ser acessado da pagina onde é aplicad

--- src/app.py
+++ src/app.py
@@ -18,62 +18,61 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("index.html", name_now=name_now)
 
 
 @app.route("/sobre_nos")
 def sobre_nos():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("sobre_nos.html", name_now=name_now)
 
 
 @app.route("/exame")
 def exame():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("exame.html", name_now=name_now)
 
 
 @app.route("/resultados")
 def resultados():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("resultados.html", name_now=name_now)
 
 
 @app.route("/artefatos-e-eventos-1")
 def artefatoseeventos1():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("conteudos/artefatos-e-eventos-1.html", name_now=name_now)
 
 
 @app.route("/introducao")
 def introducao():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("conteudos/introducao.html", name_now=name_now)
 
 
 @app.route("/artefatos-e-eventos-2")
 def artefatoseeventos2():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("conteudos/artefatos-e-eventos-2.html", name_now=name_now)
 
 
 @app.route("/papeis-e-pilares")
 def papeisepilares():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("conteudos/papeis-e-pilares.html", name_now=name_now)
 
 
 @app.route("/exemplo")
 def conteudo():
-    name_now = session.get('name_now')
+    name_now = session.get("name_now")
     return render_template("conteudos/exemplo.html", name_now=name_now)
 
 
 @app.route("/questoes")
 def questoes():
-
-    return render_template("components/questoes.html")
+    return render_template("samples/questoes.html")
 
 
 @app.route("/cadastro")
@@ -82,11 +81,10 @@ def cadastro():
     return render_template("components/questoes.html", name_now=name_now)
 
 
-@app.route('/login', methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    email = data['email']
-    
+    email = data["email"]
 
     try:
         cur = mysql.connection.cursor()
@@ -95,39 +93,42 @@ def login():
         cur.close()
 
         if user:
-            session['user_id'] = user[0]
-            session['name_now'] = user[1]
-            session['email'] = email
-            return jsonify({'success': True})
+            session["user_id"] = user[0]
+            session["name_now"] = user[1]
+            session["email"] = email
+            return jsonify({"success": True})
         else:
-            return jsonify({'success': False})
+            return jsonify({"success": False})
     except Exception as e:
-        return jsonify({'success': False})
+        return jsonify({"success": False})
 
-@app.route('/register', methods=['POST'])
+
+@app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    NameUser = data['NameUser']
-    email = data['email']
+    NameUser = data["NameUser"]
+    email = data["email"]
     created_at = datetime.datetime.now()
     if len(NameUser.split()) > 1:
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users (name, email, created_at) VALUES (%s, %s,%s)", (NameUser, email, created_at))
+        cur.execute(
+            "INSERT INTO users (name, email, created_at) VALUES (%s, %s,%s)",
+            (NameUser, email, created_at),
+        )
         mysql.connection.commit()
         cur.close()
-        session['name_now'] = NameUser
-        session['email'] = email
-        return jsonify({'NameUser': NameUser, 'email': email})
+        session["name_now"] = NameUser
+        session["email"] = email
+        return jsonify({"NameUser": NameUser, "email": email})
     else:
-        return jsonify({'success': False, 'message': 'Invalid name'}), 400
+        return jsonify({"success": False, "message": "Invalid name"}), 400
 
 
-
-@app.route('/logout', methods=['POST'])
+@app.route("/logout", methods=["POST"])
 def logout():
-    session.pop('name_now', None)
-    session.pop('email', None)
-    return jsonify({'success': True})
+    session.pop("name_now", None)
+    session.pop("email", None)
+    return jsonify({"success": True})
 
 
 @app.route("/avaliar")
@@ -135,12 +136,14 @@ def avaliar():
     name_now = session.get('name_now')
     return render_template("components/avaliar.html", name_now=name_now)
 
+
 @app.route("/certificado")
 def certificado():
     return render_template("components/certificado.html")
 
-#Quizz ainda nao funcionando pois falta o interaction_id
-@app.route('/submit-score', methods=['POST'])
+
+# Quizz ainda nao funcionando pois falta o interaction_id
+@app.route("/submit-score", methods=["POST"])
 def submit_score():
     data = request.json
     total_correct = data.get("totalCorrect")
@@ -148,16 +151,19 @@ def submit_score():
     print(f"Quantidade de questões corretas recebidas: {total_correct}")
 
     # cursor = mysql.connection.cursor()
-    # cursor.execute(''' 
-    #     INSERT INTO quizzes (score, users_answer) 
-    #     VALUES (%s, %s) 
+    # cursor.execute('''
+    #     INSERT INTO quizzes (score, users_answer)
+    #     VALUES (%s, %s)
     # ''', (total_correct, user_answers))
     # mysql.connection.commit()
     # cursor.close()
 
-    return jsonify({"status": "success", "totalCorrect": total_correct, "userAnswer": user_answers})
+    return jsonify(
+        {"status": "success", "totalCorrect": total_correct, "userAnswer": user_answers}
+    )
 
-@app.route('/submit-score-exame', methods=['POST'])
+
+@app.route("/submit-score-exame", methods=["POST"])
 def submit_score_exame():
     data = request.json
     total_correct = data.get("totalCorrect")
@@ -165,31 +171,44 @@ def submit_score_exame():
 
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute('''
+        cursor.execute(
+            """
             INSERT INTO exams (score, users_answer, created_at)
             VALUES (%s, %s, NOW())
-        ''', (total_correct, users_answer))
+        """,
+            (total_correct, users_answer),
+        )
         mysql.connection.commit()
         cursor.close()
 
-        return jsonify({"status": "success", "totalCorrect": total_correct, "userAnswers": users_answer})
+        return jsonify(
+            {
+                "status": "success",
+                "totalCorrect": total_correct,
+                "userAnswers": users_answer,
+            }
+        )
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
-    
-@app.route('/submit-avaliacao', methods=['POST'])
+
+
+@app.route("/submit-avaliacao", methods=["POST"])
 def submit_avaliacao():
-    comentario = request.form['comentario']
-    estrelas = request.form['fb']
+    comentario = request.form["comentario"]
+    estrelas = request.form["fb"]
 
     print(f"Comentário: {comentario}, Estrelas: {estrelas}")
-    
-    
+
     cursor = mysql.connection.cursor()
-    cursor.execute(''' INSERT INTO iterations(review_comment, review_score, created_at) VALUES(%s, %s, NOW()) ''', (comentario, estrelas))
+    cursor.execute(
+        """ INSERT INTO iterations(review_comment, review_score, created_at) VALUES(%s, %s, NOW()) """,
+        (comentario, estrelas),
+    )
     mysql.connection.commit()
     cursor.close()
 
-    return redirect(url_for('resultados'))
+    return redirect(url_for("resultados"))
+
 
 @app.route("/estimativas")
 def estimativas():
