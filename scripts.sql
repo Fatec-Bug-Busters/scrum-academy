9a404dd1b9e1005b3aa55ad30914548d0ae0fd22
oFelipeChagas <felipe.chagas2@fatec.sp.gov.br>
2024-06-11 12:31:32 -0300

    correção bd

--- scripts.sql
+++ scripts.sql
@@ -16,7 +16,6 @@ create table iterations (
     `total_score` int null,
     `review_score` int null,
     `review_comment` varchar(255) null,
-    `created_at` datetime not null,
     foreign key (users_id) references users(id)
 );
 
@@ -26,7 +25,6 @@ create table quizzes (
     `index` int not null,
     `score` int not null,
     `users_answer` varchar(127) not null,
-    `created_at` datetime not null,
     foreign key (iteration_id) references iterations(id)
 );
 
@@ -48,15 +46,15 @@ INSERT into users (id, name, email, created_at) VALUES
 ('11', 'Leon Trotsky', 'frida@onionproyect.com', "2024-06-02 18:01:09"),
 ('12', 'Frida Kahlo', 'leonmiamor@onioproyect.com', "2024-06-02 18:01:09");
 
-INSERT into iterations (users_id, total_score, review_score, review_comment, created_at) VALUES
-('5', '16', '5', 'El mejor sitio para agregar conocimientos en Scrum', "2024-06-02 18:01:09" ),
-('6', '14', '5', 'Aonde é que eu assino para pegar o certificado?' , "2024-06-02 18:01:09"),
-('7', '16', '4','Não sei como fui aprovado, foi um processo muito intenso e imersivo', "2024-06-02 18:01:09" ),
-('8', '12', '5', "Eu sabia que detrás de um cachorro, que estava escondido atrás da figura de um menino, havia hight tec tec", "2024-06-02 18:01:09"),
-('9', '12', '5', 'Trama típica de uma das minhas novelas!', "2024-06-02 18:01:09"),
-('10', '16', '3', 'Vocês usaram bem a matemática que eu inventei, rapazes!', "2024-06-02 18:01:09"),
-('11', '8', '2', 'Só fiz porque minha mulher mandou', "2024-06-02 18:01:09"),
-('12', '16', '4', 'O de cima é meu pau-mandado', "2024-06-02 18:01:09");
+INSERT into iterations (users_id, total_score, review_score, review_comment) VALUES
+('5', '16', '5', 'El mejor sitio para agregar conocimientos en Scrum' ),
+('6', '14', '5', 'Aonde é que eu assino para pegar o certificado?'),
+('7', '16', '4','Não sei como fui aprovado, foi um processo muito intenso e imersivo'),
+('8', '12', '5', "Eu sabia que detrás de um cachorro, que estava escondido atrás da figura de um menino, havia hight tec tec"),
+('9', '12', '5', 'Trama típica de uma das minhas novelas!'),
+('10', '16', '3', 'Vocês usaram bem a matemática que eu inventei, rapazes!'),
+('11', '8', '2', 'Só fiz porque minha mulher mandou'),
+('12', '16', '4', 'O de cima é meu pau-mandado');
 
 -- create user
 CREATE USER 'scrum'@'localhost' IDENTIFIED WITH caching_sha2_password BY '123456';
