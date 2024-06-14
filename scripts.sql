8f69c7389a169dc3b86e99d6b4c6fa21b2009f64
oFelipeChagas <felipe.chagas2@fatec.sp.gov.br>
2024-06-14 08:11:08 -0300

    repopulando o bd

--- scripts.sql
+++ scripts.sql
@@ -41,15 +41,16 @@ INSERT into users (id, name, email, created_at) VALUES
 ('11', 'Leon Trotsky', 'frida@onionproyect.com', "2024-06-02 18:01:09"),
 ('12', 'Frida Kahlo', 'leonmiamor@onioproyect.com', "2024-06-02 18:01:09");
 
--- INSERT into reviews (user_id, total_score, review_score, review_comment) VALUES
--- ('5', '16', '5', 'El mejor sitio para agregar conocimientos en Scrum' ),
--- ('6', '14', '5', 'Aonde é que eu assino para pegar o certificado?'),
--- ('7', '16', '4','Não sei como fui aprovado, foi um processo muito intenso e imersivo'),
--- ('8', '12', '5', "Eu sabia que detrás de um cachorro, que estava escondido atrás da figura de um menino, havia hight tec tec"),
--- ('9', '12', '5', 'Trama típica de uma das minhas novelas!'),
--- ('10', '16', '3', 'Vocês usaram bem a matemática que eu inventei, rapazes!'),
--- ('11', '8', '2', 'Só fiz porque minha mulher mandou'),
--- ('12', '16', '4', 'O de cima é meu pau-mandado');
+INSERT INTO exams (user_id, score, users_answer, review_score, review_comment, created_at) VALUES 
+('5', '10', '{"q1": "b", "q2": "b", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '5', 'El mejor sitio para agregar conocimientos en Scrum', "2024-06-02 18:01:09"), 
+('6', '9', '{"q1": "a", "q2": "b", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '4', 'Aonde é que eu assino para pegar o certificado?', "2024-06-02 18:01:09"), 
+('7', '8', '{"q1": "a", "q2": "a", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '5', 'Não sei como fui aprovado, foi um processo muito intenso e imersivo', "2024-06-02 18:01:09"), 
+('8', '8', '{"q1": "a", "q2": "a", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '4', 'Eu sabia que detrás de um cachorro, que estava escondido atrás da figura de um menino, havia hight tec tec', "2024-06-02 18:01:09"),  
+('9', '8', '{"q1": "a", "q2": "a", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '4', 'Trama típica de uma das minhas novelas!', "2024-06-02 18:01:09"), 
+('10', '9', '{"q1": "b", "q2": "a", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '5', 'Vocês usaram bem a matemática que eu inventei, rapazes!', "2024-06-02 18:01:09"), 
+('11', '5', '{"q1": "b", "q2": "a", "q3": "a", "q4": "a", "q5": "a", "q6": "a", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '3', 'Só fiz porque minha mulher mandou', "2024-06-02 18:01:09"), 
+('12', '10', '{"q1": "a", "q2": "a", "q3": "b", "q4": "b", "q5": "b", "q6": "b", "q7": "b", "q8": "b", "q9": "b", "q10": "b"}', '5', 'O de cima é meu pau-mandado', "2024-06-02 18:01:09");
+
 
 -- create user
 CREATE USER 'scrum'@'localhost' IDENTIFIED WITH caching_sha2_password BY '123456';
